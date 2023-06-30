578fd18929afda1dc2706c9e7b2e47230c78a382
dmutrevna <101204568+dmutrevna@users.noreply.github.com>
2023-06-30 18:22:08 +0200

    shopping-list-html-css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
+import { fetchCategoryList } from './js/api_request';
+import { fetchTopBooks } from './js/api_request';
+import { fetchCertainCategory } from './js/api_request';
 import './js/category';
-
-
-
-
-
-
+import './js/shopping-list-section';
