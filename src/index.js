d51491a38f28aa28942121e8b86ccac75269d08c
Vitalii-Dashivets <vdashivets1985@gmail.com>
2023-07-01 20:10:38 +0300

    Add scroll

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import { fetchCategoryList } from './js/api_request';
 import { fetchTopBooks } from './js/api_request';
 import { fetchCertainCategory } from './js/api_request';
 import { fetchSelectedBook } from './js/api_request';
-import './js/shopping-list-section';
-import './js/modal-window';
-import './js/category';
-import './js/mobile-menu';
-import './js/open-menu';
-
+import './js/shopping-list-section.js';
+import './js/modal-window.js';
+import './js/category.js';
+import './js/mobile-menu.js';
+import './js/open-menu.js';
+import './js/scroll-up-btn.js';
 
 
