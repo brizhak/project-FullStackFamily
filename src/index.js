040a18bc1c3dcb840b4f7045092d3c38400bd1b9
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-07-01 15:18:46 +0300

    add requests local storage

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import { fetchCategoryList } from './js/api_request';
 import { fetchTopBooks } from './js/api_request';
 import { fetchCertainCategory } from './js/api_request';
-
+import { fetchSelectedBook } from './js/api_request';
 import './js/shopping-list-section';
 
 import './js/category';
