77264e392115669e22f0022eeb2c7e0f3f7131f2
AnonymousMar <119819802+AnonymousMar@users.noreply.github.com>
2023-07-02 07:55:38 +0200

    dark theme

--- src/js/dark.js
+++ src/js/dark.js
@@ -1,19 +1,42 @@
-let theme = localStorage.getItem('theme') || 'light';
-let themeLibrary = localStorage.getItem('theme-library') || 'light-library';
-const btnSwitch = document.querySelector('.toggle-switch');
-btnSwitch.addEventListener('click', function () {
-    if (theme === 'dark') {
-        document.querySelector('body').classList.remove('dark');
-        theme = 'light';
-        themeLibrary = 'light-library';
-    } else {
-        document.querySelector('body').classList.add('dark');
-        theme = 'dark';
-        themeLibrary = 'dark-library';
-    }
-    localStorage.setItem('theme', theme);
-    localStorage.setItem('theme-library', themeLibrary);
-});
-if (theme === 'dark') {
-    document.querySelector('body').classList.add('dark');
+const btnDarkMode = document.querySelector(".switch");
+
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
 }
+
+if (localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+} else if (localStorage.getItem("darkMode") === "light") {
+    btnDarkMode.classList.remove("dark-mode-btn--active");
+    document.body.classList.remove("dark");
+}
+
+window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (event) => {
+        const newColorScheme = event.matches ? "dark" : "light";
+
+        if (newColorScheme === "dark") {
+            btnDarkMode.classList.add("dark-mode-btn--active");
+            document.body.classList.add("dark");
+            localStorage.setItem("darkMode", "dark");
+        } else {
+            btnDarkMode.classList.remove("dark-mode-btn--active");
+            document.body.classList.remove("dark");
+            localStorage.setItem("darkMode", "light");
+        }
+    });
+
+
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+    const isDark = document.body.classList.toggle("dark");
+
+    if (isDark) {
+        localStorage.setItem("darkMode", "dark");
+    } else {
+        localStorage.setItem("darkMode", "light");
+    }
+};
